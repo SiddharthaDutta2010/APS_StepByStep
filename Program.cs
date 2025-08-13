dfbfb3537f08e459942340190d45b6cd1cd928fc
SubirMerlin <subird@merlinindustries.com>
2025-08-14 01:22:43 +0530

    Step2

--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
 ﻿using Autodesk.Authentication;
 using Autodesk.Authentication.Model;
+using Newtonsoft.Json.Linq;
 
 namespace APS_StepByStep
 {
+    public record Token(string AccessToken, DateTime ExpiresAt);
     class AuthController
     {
-        public static async Task<string> GetToken(string _clientId, string _clientSecret, List<Scopes> scopes)
+        public static async Task<Token> GetToken(string _clientId, string _clientSecret, List<Scopes> scopes)
         {
             var authenticationClient = new AuthenticationClient();
             var auth = await authenticationClient.GetTwoLeggedTokenAsync(_clientId, _clientSecret, scopes);
-            return auth.AccessToken;
+            return new Token(auth.AccessToken, DateTime.UtcNow.AddSeconds((double)auth.ExpiresIn));
         }
     }
     internal class Program
