7dce6afacb296d423e2747b9dbce2d4cbcf429ba
SubirMerlin <subird@merlinindustries.com>
2025-08-14 01:17:29 +0530

    Step1

--- Program.cs
+++ Program.cs
@@ -3,13 +3,21 @@ using Autodesk.Authentication.Model;
 
 namespace APS_StepByStep
 {
+    class AuthController
+    {
+        public static async Task<string> GetToken(string _clientId, string _clientSecret, List<Scopes> scopes)
+        {
+            var authenticationClient = new AuthenticationClient();
+            var auth = await authenticationClient.GetTwoLeggedTokenAsync(_clientId, _clientSecret, scopes);
+            return auth.AccessToken;
+        }
+    }
     internal class Program
     {
         static async Task Main(string[] args)
         {
-            var authenticationClient = new AuthenticationClient();
-            var auth = await authenticationClient.GetTwoLeggedTokenAsync("dmqcHi4CsHnSoH5NZB3L6ZjTzBCaKWA2HGn26Zh9IPasxBLk", "YakHyIAogXSWsWpnGGLXPhgSkIAAYgGHEzxk4cXJZDa5x3gc69TFAbmbaNXwuq1Q", [Scopes.ViewablesRead]);
-            Console.WriteLine(auth.AccessToken);
+            var accessToken = AuthController.GetToken("dmqcHi4CsHnSoH5NZB3L6ZjTzBCaKWA2HGn26Zh9IPasxBLk", "YakHyIAogXSWsWpnGGLXPhgSkIAAYgGHEzxk4cXJZDa5x3gc69TFAbmbaNXwuq1Q", [Scopes.ViewablesRead]).Result;
+            Console.WriteLine(accessToken);
         }
     }
 }
